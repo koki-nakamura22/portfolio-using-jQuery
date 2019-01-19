a34fc26d23759f809c0b3eaae910b55bc8e49a2c
Koki Nakamura <nk.levia22@gmail.com>
2019-01-19 21:44:53 +0900

    Added webpack and webpack-dev-server config files
    Added webpack and webpack-dev-server config files.
    And added following commands to package.json.
    - Command to run webpack-dev-server
    - Command to build source code in development mode
    - Command to build source code in production mode

---
+++ config/webpack.config.development.js
@@ -0,0 +1,18 @@
+const merge = require("webpack-merge");
+const commonConfig = require("./webpack.config.common.js");
+const path = require("path");
+
+module.exports = merge(commonConfig, {
+  watch: true,
+  mode: "development",
+  devtool: "source-map",
+  devServer: {
+    hot: true,
+    inline: true,
+    contentBase: path.join(__dirname, "../public"),
+    watchContentBase: true,
+    open: "Chrome",
+    openPage: "index.html",
+    port: 18080
+  }
+});
